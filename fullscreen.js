cb096e352205efcefa8119797aee1a54bee5faf2
lll69 <60803753+lll69@users.noreply.github.com>
2022-06-14 13:23:01 +0800

    Update fullscreen.js

--- fullscreen.js
+++ fullscreen.js
@@ -3,7 +3,7 @@ var fullArea = document.querySelector("#playarea");
 fullButton.onclick = function () {
     fullArea.requestFullscreen();
 }
-document.querySelector("#canvas").oncontextmenu = null;
+// document.querySelector("#canvas").oncontextmenu = null;
 var closeButton = document.querySelector("#close");
 closeButton.onclick = function () {
     window.history.back();
